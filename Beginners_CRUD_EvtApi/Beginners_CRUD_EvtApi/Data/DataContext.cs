73ab1416ff12bfb2a32975d79df800f6bf4a5d65
Antonio Azevedo <antonio.azevedo@evtit.com>
2022-07-25 14:01:16 -0300

    First Commit New SuperHeroAPI

---
+++ Beginners_CRUD_EvtApi/Beginners_CRUD_EvtApi/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Beginners_CRUD_EvtApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+
+    }
+}
